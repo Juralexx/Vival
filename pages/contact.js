e5ae9e75c1f77b6f76bd0f3bdf83dfc3c6d676b9
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-04-04 04:35:01 +0200

    Updated contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,62 +1,34 @@
 import React from "react";
 import useSWR from 'swr'
 import { fetcher } from "functions/utils";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 import styled from "styled-components";
 import { fetchSiteDatas } from "api/site";
 import Layout from "layouts/Layout";
 import NextBreadcrumbs from "tools/Breadcrumb";
-import Icon from "icons/Icon";
-import FacebookCard from "components/FacebookCard";
+import Form from "components/Form";
 
-const LeafletMap = dynamic(() => import('components/map/LeafletMap'), { ssr: false })
+const LeafletMap = dynamic(() => import('components/LeafletMap'), { ssr: false })
 
 export default function Contact({ datas }) {
     const { data } = useSWR(process.env.SITE_API_ROUTE, fetcher, { initialData: datas })
-    const title = 'Contact'
+    const title = 'Contactez-nous'
 
     return (
         data && (
-            <Layout
-                datas={data}
-                title={title}
-            >
-                <NextBreadcrumbs
-                    denomination={data.denomination}
-                />
-                <h1>{title}</h1>
+            <Layout datas={data} title={title}>
                 <ContactContainer id="contact-page">
                     <div className="__grid">
-                        <div className="infos-container">
-                            <div className="infos">
-                                <p>
-                                    <strong>{data.denomination}</strong>, votre <strong className="lowercase">{data.job}</strong> à&nbsp;<strong>{data.city}</strong> répond à vos besoins.
-                                    Pour toute demande d'informations, contactez-nous.
-                                </p>
-                            </div>
-                            <div className="infos linked">
-                                <a href={data.googlemap} target="_blank" rel="noreferrer">
-                                    <Icon name="Map" />{data.street}<br />{data.postcode} {data.city}
-                                </a>
-                            </div>
-                            <div className="infos linked">
-                                <a href={"tel:" + data.phone}>
-                                    <Icon name="Phone" />{data.phone}
-                                </a>
-                            </div>
-                            <div className="infos linked">
-                                <Icon name="Clock" />
-                                {data.opening.map((el, key) => {
-                                    return (
-                                        <p key={key}>
-                                            <span className="font-bold">{el.day ? el.day : ''} : </span>{el.range ? el.range : ''}
-                                        </p>
-                                    )
-                                })}
-                            </div>
-                            <div className="socials-infos">
-                                <FacebookCard datas={datas} />
-                            </div>
+                        <div className="form-container">
+                            <FormContainer id='form'>
+                                <h1>{title}</h1>
+                                <NextBreadcrumbs
+                                    denomination={data.denomination}
+                                />
+                                <p>Pour tous renseignements complémentaires concernant <strong>{data.denomination}</strong>, votre <strong className="lowercase">{data.job}</strong> à&nbsp;<strong>{data.city}</strong>, contactez-nous.</p>
+                                <p className="small"><small><strong>Protection de vos données :</strong> les informations que vous nous communiquez sont confidentielles. Nous nous engageons à ne jamais les diffuser ni à les transmettre à des tiers.</small></p>
+                                <Form />
+                            </FormContainer>
                         </div>
                         <div className="contact-map">
                             <LeafletMap datas={data} />
@@ -78,80 +50,102 @@ export async function getStaticProps() {
 }
 
 const ContactContainer = styled.div`
-    position : relative;
-    width    : 100%;
-    padding  : 50px 0 150px;
-    margin   : 0 auto;
-    max-width : 1700px;
+    position  : relative;
+    width     : 100%;
+    padding   : 0;
+    max-width : 1920px;
+    margin    : 0 auto;
+
+    h1 {
+        text-align    : left;
+        margin-bottom : 0;
+    }
+    .small {
+        line-height : 1.1;
+    }
 
     .__grid {
         display               : grid;
-        grid-template-columns : 1fr 2fr;
-        max-width             : 1700px;
+        grid-template-columns : 1fr 1fr;
 
         @media(max-width: 992px) {
             grid-template-columns : 1fr;
         }
     }
 
-    .infos-container {
-        display        : flex;
-        flex-direction : column;
-        padding        : 0 50px;
+    .form-container {
+        position : relative;
+        height   : 100%;
+        padding  : 100px;
+        z-index  : 2;
 
-        @media(max-width: 992px) {
-            max-width : 400px;
-            margin    : 0 auto 70px;
+        @media(max-width: 1366px) {
+            padding  : 100px 50px;
         }
-
-        .infos {
-            position       : relative;
-            display        : flex;
-            flex-direction : column;
-            align-items    : flex-start;
-            margin         : 10px 0;
-
-            &.linked {
-                margin-left : 25px;
-            }
-
-            svg {
-                position     : absolute;
-                top          : 4px;
-                left         : -25px;
-                height       : 15px;
-                width        : 15px;
-                margin-right : 5px;
-                color        : var(--text-xlight);
-            }
+        @media(max-width: 1200px) {
+            padding  : 50px 30px;
         }
-
-        .socials-infos {
-            div {
-                margin : 20px 0;
-            }
-        }
-    
-        p {
-            position   : relative;
-            text-align : left;
+        @media(max-width: 768px) {
+            padding : 50px 15px;
         }
     }
 
     .contact-map {
-        border-radius : var(--rounded-md);
-        padding       : 0;
-        box-shadow: inset var(--shadow-smooth);
+        position   : relative;
+        padding    : 100px 0;
+        height     : 100%;
+        z-index    : 0;
+        .leaflet-map,
         .leaflet-container {
-            height        : 600px;
-            border-radius : var(--rounded-md);
+            height     : 100%;
+            box-shadow : var(--shadow-smooth);
+        }
+        @media(max-width: 1366px) {
+            padding  : 100px 50px;
+        }
+        @media(max-width: 1200px) {
+            padding  : 50px 30px;
+        }
+        @media(max-width: 992px) {
+            height : 600px;
+        }
+        @media(min-width:577px) and (max-width: 992px) {
+            margin-top : -150px;
+        }
+        @media(max-width: 768px) {
+            padding : 50px 15px;
         }
     }
+`
 
-    @media(max-width: 992px) {
-        padding : 0;
-        .infos-container {
-            padding : 0 15px;
+const FormContainer = styled.div`
+    position         : relative;
+    width            : 100%;
+    background-color : var(--body);
+
+    @media(min-width:577px) and (max-width: 992px) {
+        width         : 90%;
+        margin        : 0 auto;
+        padding       : 20px 30px;
+        border-radius : var(--rounded-md);
+        box-shadow    : var(--shadow-bottom);
+    }
+
+    .breadcrumb {
+        display      : inline-block;
+        margin-right : auto;
+        padding-top  : 0;
+    }
+
+    .btn__container {
+        justify-content : center;
+    }
+
+    .dynamic-input,
+    .textarea {
+        background-color : var(--green);
+        label {
+            background-color : var(--green);
         }
     }
 `
