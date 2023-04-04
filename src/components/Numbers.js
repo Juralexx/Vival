84ec647f3f3f1ed7f5adcae6feac243df1952cf4
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-04-04 04:40:19 +0200

    Updated Number.js

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,48 +1,54 @@
 import React from "react";
+import { usePathname } from "next/navigation";
 import styled from "styled-components";
 import { Revealer } from 'tools/Revealer'
 import Icon from "./tools/icons/Icon";
 
-const Numbers = () => {
+const Numbers = ({ datas }) => {
+    const pathname = usePathname()
+
     return (
-        <NumbersContainer className="container" id="in-numbers">
-            <h2>En quelques chiffres</h2>
-            <div className="in-numbers-container">
-                <Revealer className="number">
-                    <div className="icon-container">
-                        <Icon name="Medal" />
-                    </div>
-                    <p className="number-title">
-                        <span>6</span> ans <br /> d'expérience
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    </p>
-                </Revealer>
-
-                <Revealer delay={300} className="number">
-                    <div className="icon-container">
-                        <Icon name="ShoppingBasket" />
-                    </div>
-                    <p className="number-title">
-                        <span>1200</span >+ <br /> références
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    </p>
-                </Revealer>
-
-                <Revealer delay={400} className="number">
-                    <div className="icon-container">
-                        <Icon name="Percent" />
-                    </div>
-                    <p className="number-title">
-                        <span>100</span>% <br /> de client satisfait
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    </p>
-                </Revealer>
+        <NumbersContainer id="in-numbers" pathname={pathname}>
+            <div className="container-lg">
+                <h2>En quelques chiffres</h2>
+                <div className="__flex">
+                    <Revealer className="number">
+                        <div className="icon-container">
+                            <Icon name="Medal" />
+                        </div>
+                        <p className="number-title">
+                            <span>7</span> ans <br /> d'expérience
+                        </p>
+                        <p>Votre épicerie <strong>Vival Meillonnas</strong> est ouverte <strong>depuis 2016</strong>.</p>
+                    </Revealer>
+                    <Revealer delay={300} className="number">
+                        <div className="icon-container">
+                            <Icon name="ShoppingBasket" />
+                        </div>
+                        <p className="number-title">
+                            <span>1200</span >+ <br /> références
+                        </p>
+                        <p>Un large choix de produits de qualité, frais et de saison.</p>
+                    </Revealer>
+                    <Revealer delay={400} className="number">
+                        <div className="icon-container">
+                            <Icon name="Percent" />
+                        </div>
+                        <p className="number-title">
+                            <span>98</span>% <br /> de client satisfait
+                        </p>
+                        <p>Basé sur les <a href='https://bit.ly/3Ts2d7L' target='_blank' className="custom-link">avis Google</a>.</p>
+                    </Revealer>
+                    <Revealer delay={500} className="number">
+                        <div className="img-container">
+                            <img src='/img/meilleure-chaine-de-magasins-2023.jpg' />
+                        </div>
+                        <p className="number-title">
+                            Meilleure chaine de<br /> magasin de l'année
+                        </p>
+                        <p>Vival a été élus meilleure chaine de magasins de l'année 2023, catégorie «&nbsp;Commerces&nbsp;de&nbsp;proximité&nbsp;»</p>
+                    </Revealer>
+                </div>
             </div>
         </NumbersContainer>
     )
@@ -51,27 +57,46 @@ const Numbers = () => {
 export default Numbers
 
 const NumbersContainer = styled.div`
-    padding : 50px 0;
+    position   : relative;
+    padding    : 50px 0;
+    background : ${props => props.pathname !== '/' && 'rgba(var(--primary-rgb), 0.02)'};
+
+    &:after {
+        position   : absolute;
+        content    : "";
+        top        : -100px;
+        left       : 0;
+        right      : 0;
+        height     : 100px;
+        width      : 100%;
+        z-index    : -1;
+        background : ${props => props.pathname !== '/' && 'rgba(var(--primary-rgb), 0.02)'};
+        clip-path  : polygon(100% 0, 0% 100%, 100% 100%);
+    }
+
+    .container-lg {
+        padding : 0;
+    }
 
     h2 {
         text-align    : center;
         margin-bottom : 50px;
     }
 
-    .in-numbers-container {
+    .__flex {
         display         : flex;
         justify-content : center;
         flex-wrap       : wrap;
 
         .number {
             position      : relative;
-            padding       : 43px 15px 20px;
+            padding       : 0 12px;
             text-align    : center;
             margin        : 30px auto;
-            max-width     : 300px;
-            border        : 1px solid var(--light-border);
-            box-shadow    : var(--shadow-colored);
-            border-radius : var(--rounded-3xl);
+            min-width     : 25%;
+            width         : 100%;
+            max-width     : 330px;
+            border-radius : var(--rounded-xl);
 
             .number-title {
                 color       : var(--primary);
@@ -79,7 +104,7 @@ const NumbersContainer = styled.div`
                 font-weight : 600;
                 white-space : nowrap;
                 padding     : 10px 0;
-                margin-top  : 7px;
+                margin-top  : 20px;
     
                 span {
                     font-size   : 45px;
@@ -88,25 +113,33 @@ const NumbersContainer = styled.div`
             }
 
             .icon-container {
-                position      : absolute;
-                top           : -42.5px;
-                left          : 50%;
-                transform     : translateX(-50%);
+                position      : relative;
                 margin        : 0 auto;
-                width         : 85px;
-                height        : 85px;
-                background    : var(--body);
+                width         : 110px;
+                height        : 110px;
+                background    : var(--green);
                 border-radius : var(--rounded-full);
-                border        : 1px solid var(--light-border);
-                box-shadow    : var(--shadow-tiny);
             }
-    
+
+            .img-container {
+                position : relative;
+                margin   : 0 auto;
+                width    : 110px;
+                height   : 110px;
+            }
+
             svg {
                 width         : 100%;
                 height        : 100%;
-                padding       : 15px;
+                padding       : 18px;
                 color         : var(--secondary);
                 border-radius : var(--rounded-full);
+            }
+
+            img {
+                max-height    : 130px;
+                width         : auto;
+                border-radius : var(--rounded-md);
             }
         }
     }
