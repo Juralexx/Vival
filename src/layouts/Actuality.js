f3e4b966a0ee9956558fd2f34bd2c1fcbf1b9678
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-04-04 04:38:19 +0200

    Updated Actuality.js

--- src/layouts/Actuality.js
+++ src/layouts/Actuality.js
@@ -1,20 +1,31 @@
 import React from "react"
-import Brands from "components/Brands"
-import Map from "components/map/Map"
+import Images from "components/Images"
 import Parallax from "components/Parallax"
-import ContactBar from "components/ContactBar"
+import Informations from "components/Informations"
+import Contact from "components/Contact"
+import Numbers from "components/Numbers"
 
-const ActualityLayout = ({ children, datas }) => {
+const ActualityLayout = ({ children, datas, brands, partners }) => {
     return (
         datas &&
         <React.Fragment>
             {children}
-            <ContactBar
+            <Informations
                 datas={datas}
             />
             <Parallax />
-            <Brands />
-            <Map
+            <Numbers
+                datas={datas}
+            />
+            <Images
+                title="Nos Partenaires"
+                images={partners}
+            />
+            <Images
+                title="Nos Marques"
+                images={brands}
+            />
+            <Contact
                 datas={datas}
             />
         </React.Fragment>
