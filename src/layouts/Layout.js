665e3e92f1e76984935e6882c9171534d48ba95c
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-04-04 04:39:01 +0200

    Updated Layout.js

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -5,42 +5,45 @@ import Footer from './Footer'
 import styled from 'styled-components'
 import ScrolltoTop from 'tools/Scrolltotop'
 import Share from 'tools/Share'
-import { addClass } from 'functions/utils'
+import { addClass, convertStringToURL } from 'functions/utils'
 import HeaderSlim from 'layouts/HeaderSlim'
 import Header from 'layouts/Header'
 
 export default function Layout({ children, datas, title, headerImg }) {
     const pathname = usePathname()
-    
+    const classes = pathname.replace(/\//g, " ").trim()
+
     return (
         datas &&
-        <main className={addClass(pathname === '/', 'main')}>
-            <HeadProvider
-                datas={datas}
-                title={title && `${title} - ${datas.denomination} - ${datas.job} à ${datas.city}`}
-                description={title && `${title} - ${datas.denomination} - ${datas.job} à ${datas.city}`}
-            />
-            <Main>
-                <Navbar datas={datas} />
-                {pathname === '/' ? (
-                    <Header datas={datas} />
-                ) : (
-                    <HeaderSlim datas={datas} image={headerImg ? `${process.env.SERVER_URL}${headerImg}` : null} />
-                )}
-                {children}
-                <CallToAction>
-                    <Share />
-                    <ScrolltoTop />
-                </CallToAction>
-            </Main>
+        <div className={addClass(pathname === '/', 'main')}>
+            <main className={`${convertStringToURL(title || 'home')} ${convertStringToURL(datas.denomination)} ${classes || 'root'}`}>
+                <HeadProvider
+                    datas={datas}
+                    title={title && `${title} - ${datas.denomination} - ${datas.job} à ${datas.city}`}
+                    description={title && `${title} - ${datas.denomination} - ${datas.job} à ${datas.city}`}
+                />  
+                <Main>
+                    <Navbar datas={datas} />
+                    {pathname === '/' ? (
+                        <Header datas={datas} />
+                    ) : (
+                        <HeaderSlim datas={datas} image={headerImg ? `${process.env.SERVER_URL}${headerImg}` : null} />
+                    )}
+                    {children}
+                    <CallToAction>
+                        <Share />
+                        <ScrolltoTop />
+                    </CallToAction>
+                </Main>
+            </main>
             <Footer datas={datas} />
-        </main>
+        </div>
     )
 }
 
 const Main = styled.div`
     width      : 100vw;
-    min-height : 100vh;
+    min-height : 1080px;
 `
 
 const CallToAction = styled.div`
