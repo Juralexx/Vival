849feb900ee089173cc4228112dcc566f41ce7c7
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-04-04 04:35:07 +0200

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,22 @@ import React from 'react'
 import useSWR from 'swr'
 import { fetcher } from 'functions/utils'
 import Bricks from 'components/Bricks'
-import CheckerboardModern from 'components/checkerboards/CheckerboardModern'
+import CheckerboardsTop from 'components/CheckerboardsTop'
 import Intro from 'components/Intro'
-import Checkerboard from 'components/checkerboards/Checkerboard'
-import Brands from 'components/Brands'
+import Checkerboard from 'components/Checkerboard'
+import Images from 'components/Images'
 import Numbers from 'components/Numbers'
+import Delivery from 'components/Delivery'
 import Parallax from 'components/Parallax'
-import ContactBar from 'components/ContactBar'
-import Map from 'components/map/Map'
+import Informations from 'components/Informations'
 import ActualityCarousel from 'components/actuality-carousel/ActualityCarousel'
+import Contact from 'components/Contact'
 import Layout from 'layouts/Layout'
-import { fetchSiteDatas } from 'api/site'
-import { getActualities } from 'api/actuality'
 import { getHome } from 'api/home'
+import { getActualities } from 'api/actuality'
+import { fetchSiteDatas, getImages } from 'api/site'
 
-export default function Home({ datas, home, actualities }) {
+export default function Home({ datas, home, actualities, brands, partners }) {
     const { data: siteDatas } = useSWR(process.env.SITE_API_ROUTE, fetcher, { initialData: datas })
     const { data: homeDatas } = useSWR(process.env.HOME_API_ROUTE, fetcher, { initialData: home })
     const { data: actuDatas } = useSWR(`${process.env.ACTUALITIES_API_ROUTE}`, fetcher, { initialData: actualities })
@@ -25,7 +26,9 @@ export default function Home({ datas, home, actualities }) {
         siteDatas &&
         homeDatas &&
         actuDatas?.data && (
-            <Layout datas={siteDatas}>
+            <Layout
+                datas={siteDatas}
+            >
                 <Bricks
                     datas={homeDatas}
                 />
@@ -33,9 +36,10 @@ export default function Home({ datas, home, actualities }) {
                     datas={siteDatas}
                     introduction={home.introduction}
                 />
-                <CheckerboardModern
-                    checkerboards={homeDatas.checkerboards.slice(0, 3)}
+                <CheckerboardsTop
+                    checkerboards={homeDatas.checkerboards.slice(0, 4)}
                 />
+                <Delivery />
                 {actuDatas.data &&
                     <ActualityCarousel
                         actualities={actuDatas.data.sort((a, b) => {
@@ -43,20 +47,29 @@ export default function Home({ datas, home, actualities }) {
                         }).slice(0, 6)}
                     />
                 }
-                <ContactBar
+                <Informations
                     datas={siteDatas}
                 />
                 <Parallax
                     datas={homeDatas}
                 />
-                <Numbers />
+                <Numbers
+                    datas={siteDatas}
+                />
                 {homeDatas.checkerboards.length > 3 &&
                     <Checkerboard
-                        checkerboards={homeDatas.checkerboards.slice(3, home.checkerboards.length)}
+                        checkerboards={homeDatas.checkerboards.slice(4, home.checkerboards.length)}
                     />
                 }
-                <Brands />
-                <Map
+                <Images
+                    title="Nos Partenaires"
+                    images={partners}
+                />
+                <Images
+                    title="Nos Marques"
+                    images={brands}
+                />
+                <Contact
                     datas={siteDatas}
                 />
             </Layout>
@@ -66,13 +79,18 @@ export default function Home({ datas, home, actualities }) {
 
 export async function getStaticProps() {
     const { siteDatas } = await fetchSiteDatas()
-    const { actualities } = await getActualities()
     const { home } = await getHome()
+    const { actualities } = await getActualities()
+    const { files: brands } = await getImages({ folder: 'img/brands' })
+    const { files: partners } = await getImages({ folder: 'img/partners' })
+
     return {
         props: {
             datas: siteDatas,
             home: home,
-            actualities: actualities
+            actualities: actualities,
+            brands: brands,
+            partners: partners
         },
     };
 }
